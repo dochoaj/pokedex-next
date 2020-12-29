7d0e012c7c7a75b3e755d3859619724e62774502
aileen <aileen.esparza12@gmail.com>
2020-12-29 12:09:17 -0300

    cambio en imágenes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import Axios from 'axios'
+import Image from 'next/image'
 //import not_found from '../public/not_found.jpg';
 const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
-const not_found='https://ih1.redbubble.net/image.731955034.9007/flat,375x500,075,f.u3.jpg'
-const logo= 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png'
 
 const fetchPokemon = async (name) => {
   if (name === '' || name === null) {
@@ -38,9 +37,9 @@ function ListAttributes({items,attr,attrTitle}) {
   );
 }
 
-function Image(picture){
+function ImageWrapper(picture){
 	return 	 (<div className="flex flex-row justify-center items-center">
-				<img src={picture.name}/> 
+				<Image src={picture.name} /> 
 			  </div>)
 }
 function Information(pokemon) {
@@ -78,7 +77,7 @@ export default function Home() {
 
 		        <div className="justify-left items-left flex-1">
 						<div>
-							<img src={logo} height='180' width='180'/> 
+							<Image src='/logo.png' /> 
 						</div>
 						<input
 						  value={name}
@@ -92,7 +91,7 @@ export default function Home() {
 								  setPokemon(pokemonData)
 							  }
 							  { pokemonData==404 && 
-								  setPicture(not_found)
+								  setPicture('/not_found.png')
 							  }
 							}
 						  }}
@@ -108,7 +107,7 @@ export default function Home() {
 					<div className='flex flex-row justify-center items-center'>	
 					  { picture && pokemon &&		    			
 							<div className="bg-white shadow overflow-hidden sm:rounded-lg">
-								<Image name={picture}/>
+								<ImageWrapper name={picture}/>
 								{pokemon!=404 &&
 									<div className="border-t border-gray-200">
 										<dl>
